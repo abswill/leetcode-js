b44a6188d38274b9eb21ff41faa1f311047f7d6c
abswill <codingbox66@gmail.com>
2020-07-01 22:30:05 +0800

    feat: 更新文案

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,31 @@
 module.exports = {
-    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
+    title: 'leetcode-js', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '算法记录', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
-        ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
+        ['link', { rel: 'icon', href: '/FE.png' }],  //浏览器的标签栏的网页图标
     ],
     markdown: {
         lineNumbers: true
     },
     serviceWorker: true,
     themeConfig: {
-        logo: '/egg.png',
+        logo: '/FE.png',
         base: '/leetcode-js/',
         lastUpdated: 'lastUpdate', // string | boolean
         nav: [
             { text: '首页', link: '/' },
-            { text: 'Github', link: 'https://github.com/abswill/abswill.github.io' },
+            { text: 'Github', link: 'https://github.com/abswill/leetcode-js' },
         ],
         sidebar: {
-            '/pages/folder1/':[
+            '/pages/easy/':[
                 {
-                    title: '测试菜单1',   // 必要的
+                    title: '简单难度',   // 必要的
                     collapsable: false, // 可选的, 默认值是 true,
                     sidebarDepth: 1,    // 可选的, 默认值是 1
                     children: [
                         ['test1.md', '子菜单1'],
-                        ['test3.md', '子菜单2']
+                        // ['test3.md', '子菜单2']
                     ]
                 }
             ],
